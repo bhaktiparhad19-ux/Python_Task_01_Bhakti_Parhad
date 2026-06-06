e8df1c1028dcebf8e173d8381693dd61574d62fc
Bhakti Parhad <bhaktiparhad19@gmail.com>
2026-06-06 12:48:28 +0530

    Delete user_input.py

--- user_input.py
+++
@@ -1,7 +0,0 @@
-name = input("Enter your name: ")
-age = input("Enter your age: ")
-technology = input("Enter your favorite technology: ")
-
-print("\nHello", name + "!")
-print("You are", age, "years old.")
-print("Your favorite technology is", technology)
