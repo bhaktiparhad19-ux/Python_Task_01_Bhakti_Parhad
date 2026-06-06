c4a0451f98edb0d24ef11f362a268e6c955baf24
Bhakti Parhad <bhaktiparhad19@gmail.com>
2026-06-06 12:48:02 +0530

    Delete challenge.py

--- challenge.py
+++
@@ -1,8 +0,0 @@
-import os
-import platform
-import sys
-
-print("Operating System:", platform.system())
-print("Username:", os.getlogin())
-print("Current Directory:", os.getcwd())
-print("Python Version:", sys.version)
