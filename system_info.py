b19e7f98cd4be88b1bcc78159589fc9866692d17
Bhakti Parhad <bhaktiparhad19@gmail.com>
2026-06-06 12:48:16 +0530

    Delete system_info.py

--- system_info.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-
-print("Name: Bhakti Parhad")
-print("College: XYZ College")
-print("Branch: Cyber Security")
-print("Internship: White Band Associates Summer Internship 2026")
-print("Date & Time:", datetime.now())
